01f265c4c0c0d7de0cb97acbea231f309d4f0b6f
No1Cheats <8zgduv2alewz>
2020-03-23 20:01:55 +0100

    Some changes, still doesn't work

--- src/main/java/todo/controller/MediaTypeFilter.java
+++ src/main/java/todo/controller/MediaTypeFilter.java
@@ -1,9 +1,12 @@
 package todo.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.owasp.html.Sanitizers;
+import todo.model.todos.Todo;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -14,17 +17,18 @@ import java.util.Enumeration;
 @WebFilter(urlPatterns = "/api/todos/*")
 public class MediaTypeFilter extends HttpFilter {
 
+    private ObjectMapper objectMapper = ObjectMapperFactory.createObjectMapper();
+
     @Override
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         if (request.getMethod().equals("POST")) {
             request.setCharacterEncoding("UTF-8");
-            Enumeration<String> e = request.getParameterNames();
-            while (e.hasMoreElements()) {
-                String value = request.getParameter(e.nextElement());
-                if (!Sanitizers.FORMATTING.and(Sanitizers.LINKS).sanitize(value).equals(value)) {
-                    response.sendRedirect("error.html");
-                    return;
-                }
+            String value = request.getContentType();
+            //Don't know where to add Accept???
+            if (!Sanitizers.FORMATTING.and(Sanitizers.LINKS).sanitize(value).equals(value)) {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.sendRedirect("");
+                return;
             }
         }
         chain.doFilter(request, response);
