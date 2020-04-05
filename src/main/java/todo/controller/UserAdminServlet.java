5872908756c85a018c0bd4c7c011b4f54c5e24ed
No1Cheats <jan.imhof@gmail.com>
2020-04-05 13:31:19 +0200

    Added the ability to register a new user

--- src/main/java/todo/controller/UserAdminServlet.java
+++ src/main/java/todo/controller/UserAdminServlet.java
@@ -1,4 +1,38 @@
 package todo.controller;
 
-public class UserAdminServlet {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import todo.model.todos.Todo;
+import todo.model.users.User;
+import todo.model.users.UserAdmin;
+import todo.model.users.UserAlreadyExistsException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/api/users/*")
+public class UserAdminServlet extends HttpServlet {
+
+    private UserAdmin userAdmin = UserAdmin.getInstance(); //Singleton implementation
+    private ObjectMapper objectMapper = ObjectMapperFactory.createObjectMapper();
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try{
+            User newUser = objectMapper.readValue(request.getInputStream(), User.class);
+            userAdmin.registerUser(newUser.getName(), newUser.getPassword());
+            objectMapper.writeValue(response.getOutputStream(), "User " + newUser.getName() + " successfully created");
+        } catch (UserAlreadyExistsException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setContentType("application/json");
+            e.printStackTrace();
+            return;
+        }
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json");
+    }
+
 }
